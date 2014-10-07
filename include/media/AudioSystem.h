boundarydevices/frameworks_av

ENGR00234072 failed to play multichannel audio repeatly
This issue caused by the gapless playback feature, which is no fit for
multichannel audio.
When the track use the direct output, it can not be used for recycle.
After finishing playback , the direct output must be released, and
in beginning of playback it will be recreated.
In cycle playback, if the next track also need to create direct output,
but previous one is not released, it will cause issue.
Here we checking the output flag, if it is direct output, release
the track first.

Change-Id: I4c2c6352003c979cb626e392688e5b932f54c52f
Signed-off-by: b02247 <b02247@freescale.com>

include/media/AudioSystem.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+** Copyright (C) 2012 Freescale Semiconductor, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -98,6 +99,9 @@ public:
     static status_t getFrameCount(audio_io_handle_t output,
                                   audio_stream_type_t stream,
                                   int* frameCount);
+    static status_t getFlags(audio_io_handle_t output,
+                                  audio_stream_type_t streamType,
+                                  int* flags);
     // returns the audio output stream latency in ms. Corresponds to
     // audio_stream_out->get_latency()
     static status_t getLatency(audio_io_handle_t output,
@@ -152,13 +156,14 @@ public:
     class OutputDescriptor {
     public:
         OutputDescriptor()
-        : samplingRate(0), format(AUDIO_FORMAT_DEFAULT), channels(0), frameCount(0), latency(0)  {}
+        : samplingRate(0), format(AUDIO_FORMAT_DEFAULT), channels(0), frameCount(0), latency(0), flags(AUDIO_OUTPUT_FLAG_NONE) {}
 
         uint32_t samplingRate;
         int32_t format;
         int32_t channels;
         size_t frameCount;
         uint32_t latency;
+        audio_output_flags_t flags;
     };
 
     // Events used to synchronize actions between audio sessions.

cuboxi/android_frameworks_av

include/media/AudioSystem.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+** Copyright (C) 2012 Freescale Semiconductor, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -104,6 +105,9 @@ public:
     static status_t getFrameCount(audio_io_handle_t output,
                                   audio_stream_type_t stream,
                                   size_t* frameCount);
+    static status_t getFlags(audio_io_handle_t output,
+                                  audio_stream_type_t streamType,
+                                  int* flags);
     // returns the audio output stream latency in ms. Corresponds to
     // audio_stream_out->get_latency()
     static status_t getLatency(audio_io_handle_t output,
@@ -155,13 +159,14 @@ public:
     class OutputDescriptor {
     public:
         OutputDescriptor()
-        : samplingRate(0), format(AUDIO_FORMAT_DEFAULT), channelMask(0), frameCount(0), latency(0)  {}
+        : samplingRate(0), format(AUDIO_FORMAT_DEFAULT), channelMask(0), frameCount(0), latency(0), flags(AUDIO_OUTPUT_FLAG_NONE) {}
 
         uint32_t samplingRate;
         audio_format_t format;
         audio_channel_mask_t channelMask;
         size_t frameCount;
         uint32_t latency;
+        audio_output_flags_t flags;
     };
 
     // Events used to synchronize actions between audio sessions.